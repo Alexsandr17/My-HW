73fdc0bebebf838f3de14c7f65e3e2a5b698aa3b
Alexsandr 17 <SkarabeiGitHub@gmail.com>
2023-02-14 20:15:12 +0300

    Зелье исправлена последняя строка

--- HM1/src/Main.java
+++ HM1/src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
 
         //todo реализовать ввод пользователем кол-ва ингредиентов.
-        boolean isTop = true;
-        while (isTop){
+        boolean isTop = false;
+
         System.out.println("****\tДобро пожаловать в программу создания элтксиров\t***");
         System.out.println("Сегодня можете сварить:\n1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости\n" +
                 "2. 2 кости ворона + 4 пельменя - Эликсир стойкости\n" +
@@ -30,20 +30,30 @@ public class Main {
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
         //todo дописать логику программы сюда
-        if (first == 3 &&second == 1)
-        System.out.println("Вы можете сварить: Эликсир зоркости");
-        if (first == 0 && second == 2 && third == 0 && fourth == 4)
+        if (first >= 3 &&second >= 1) {
+            System.out.println("Вы можете сварить: Эликсир зоркости");
+            isTop = true;
+        }
+        if (first >= 0 && second >= 2 && third >= 0 && fourth >= 4) {
             System.out.println("Вы можете сварить: Эликсир стойкости");
-        if (first == 2 && second == 0 && third == 7 && fourth == 1)
+            isTop = true;
+        }
+        if (first >= 2 && second >= 0 && third >= 7 && fourth >= 1) {
             System.out.println("Вы можете сварить: Эликсир скрытности");
-        if (first == 4 &&second ==3 && third == 5 && fourth ==10)
+            isTop = true;
+        }
+        if (first >= 4 &&second >=3 && third >= 5 && fourth >=10) {
             System.out.println("Вы можете сварить: Запретный Эликсир ");
-        else {
+            isTop = true;
+        }
+        if (isTop == false) {
             System.out.println("Не хвотает не на один эликсир");
-            isTop = false;
-        }}
-
         }
 
-    }
+        }
+}
+
+
+
+
 
